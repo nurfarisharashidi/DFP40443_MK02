25131d63b3b29251a32d0a77da658abd16d225da
FARISHARASHIDI <nurfarisharashidi@gmail.com>
2026-01-20 21:19:16 +0800

    pengasingan kod

---
+++ biodata.php
@@ -0,0 +1,5 @@
+<?php
+$nama = "NUR FARISHA BINTI RASHIDI";
+$nomatrik = "18DIT24F1151";
+$kelas = "DIT4E";
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+$nama = "NUR FARISHA BINTI RASHIDI";
+$nomatrik = "18DIT24F1151";
+$kelas = "DIT4E";
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Biodata Diri Saya</title>
+
+         <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Biodata Diri Saya</h1>
+
+    <p>Nama:<?=$nama?></p>
+    <p>No.Matrik:<?=$nomatrik?></p>
+    <p>Kelas:<?=$kelas?></p>
+    <p>Jantina:PEREMPUAN</p>
+    <p>Hobi:MEMBACA NOVEL</p>
+    <p>Tempat Lahir:KEDAH</p>
+
+    <img src="profile.jpg" alt="Gambar Profil">
+</body>
+</html>
+    
